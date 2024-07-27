5a3e03c7bfad5a513e388b5b0b88b52ffbec8324
Anas Al-Ward <anasalwardtech@gmail.com>
2024-07-27 23:34:16 +0300

    set shop app

--- FindAny/FindAny/settings.py
+++ FindAny/FindAny/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
 
     # apps
     'home',
+    'shop',
 ]
 
 MIDDLEWARE = [
--- FindAny/FindAny/urls.py
+++ FindAny/FindAny/urls.py
@@ -8,6 +8,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    path('shop/', include('shop.urls')),
 ]
 
 if settings.DEBUG:
