edcc7de89914da45002b388f18370f795ee3a2ff
Anas Al-Ward <anasalwardtech@gmail.com>
2024-07-28 02:58:27 +0300

    fix problem rm media

--- FindAny/shop/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-app_name = 'shop'
-urlpatterns = [
-    path('', views.ProductListView.as_view(), name='product_list'),
-    path('<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
-]
